fa1ea60d4c4137730c6ca20304f1b6575d4d3bb6
Tuan Ngo <tngo25@dxc.com>
2020-01-07 14:25:37 +0700

    tech6 router configuration

--- aadconnect-msgraph-nodejs-app/api/app.js
+++ aadconnect-msgraph-nodejs-app/api/app.js
@@ -90,6 +90,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
 var calendarRouter = require('./routes/calendar');
+var tech6SPRouter = require('./routes/tech6');
 
 var session = require('express-session');
 
@@ -140,5 +141,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
 app.use('/calendar', calendarRouter);
+app.use('/tech6', tech6SPRouter);
 
 module.exports = app;
