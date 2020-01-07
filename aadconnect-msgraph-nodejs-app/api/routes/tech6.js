a80f6929964083478299ca21fb4481c7b093b470
Tuan Ngo <tngo25@dxc.com>
2020-01-07 14:05:17 +0700

    add new file for Tech6 SP

---
+++ aadconnect-msgraph-nodejs-app/api/routes/tech6.js
@@ -0,0 +1,7 @@
+// DXC Tech6 items updates being maintained via SP list
+
+const express = require('express');
+const router = express.Router();
+const tokens = require('../tokens.js');
+const graph = require('../graph.js');
+
